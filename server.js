f01f8a50eea8d925d01e99c7fd9b315e5adbd826
Suryahadiningrat <rifkysuryakusuma@floothink.com>
2026-01-23 14:29:29 +0700

    fix error in server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const prisma = require('./utils/prisma');
 const analyticsRoutes = require('./routes/analyticsRoutes');
 const userRoutes = require('./routes/userRoutes');
 const projectRoutes = require('./routes/projectRoutes');
+const aiService = require('./services/aiService');
 
 const app = express();
 const PORT = process.env.PORT || 3018;
